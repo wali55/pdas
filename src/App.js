3cd38d3268d17ee57b41900bfcf61a4b50c7d115
wali55 <walisantunu@gmail.com>
2023-04-07 22:27:30 +0600

    image added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
 
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,11 @@
-function ProfileCard() {
-    return <div>Profile Card</div>;
+function ProfileCard({ title, handle, image }) {
+    return (
+        <div>
+            <img src={image} alt="pda logo"/>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    );
 }
 
 export default ProfileCard;

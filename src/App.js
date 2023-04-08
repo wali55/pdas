352d0478b45f55a1a2b7cc6033545c1e03777ebc
wali55 <walisantunu@gmail.com>
2023-04-08 14:55:55 +0600

    styling and description added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -6,11 +7,33 @@ import SiriImage from './images/siri.png';
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
 
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}
+                            description="Alexa was created by Amazon that helps you by things."
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}
+                            description="Cortana was created by Microsoft. It is very useful."
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Siri" handle="@siri01" image={SiriImage}
+                            description="Siri was made by Apple. It is very very useful as well."
+                            /> 
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
